2f3923bdc86788e0b196fccb74bef7b4923e4455
Hellen Reis <94926145+hellencreis@users.noreply.github.com>
2021-12-01 16:03:35 -0300

    [UPDATE] funcoes.sh

--- shell-script/funcoes.sh
+++ shell-script/funcoes.sh
@@ -3,18 +3,17 @@
 #2.1.Desenvolva uma função chamada lista_arquivos que retorne todos os arquivos dentro de um diretório
 #(inclusive nos sub-diretórios deste diretório) e armazene o caminho completo até estes arquivos em um vetor;
 
-function lista_arquivos(){
-diretorio=(`find $dir -type f`)
-for ((cont=0;cont<${#diretorio[@]}; cont++));
-do
-	echo "-> ${diretorio[$cont]}";
-done
+function lista_arquivos()
+{
+	diretorio=(`find $1 -type f`)
+	echo ${diretorio[*]}
 }
-
 
 #2.2.Desenvolva uma função chamada insere_texto que, dado o caminho completo para um arquivo,
 #escreva um texto qualquer no final deste arquivo
 
-function insere_texto(){
-$texto >> $arq
+function insere_texto()
+{
+	echo $1 >> $2
+	echo "Texto inserido: '$1' em $2"
 }
